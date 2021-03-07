3b574c40faf408b22d50315aaa16019cba544298
Jose Manuel Rosales <Josemanuelrosales2@gmail.com>
2021-03-07 08:27:30 -0600

    Quite los - el valor de author del objeto y agregue target blank en los botones de redes sociales

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,18 +7,18 @@ const shareTwitter = document.querySelector("#tweet-quote");
 const shareTumblr = document.querySelector("#tumblr-quote");
 
 let arrPhrases = [
-    {phrase: "Sólo vives una vez, pero si lo haces bien, una vez es suficiente.",author: "- Mae West"}, 
-    {phrase: "Tu tiempo es limitado, así que no lo desperdicies viviendo la vida de otra persona.", author:"- Steve Jobs"},
-    {phrase: "Vive como si fueras a morir mañana. Aprende como si fueras a vivir para siempre.", author: "- Mahatma Gandhi"},
-    {phrase: "No cuentes tus días, haz que tus días cuenten.", author: "-  Muhammad Ali"},
-    {phrase: "La vida no se trata de encontrarte a ti mismo. La vida se trata de crearte a ti mismo.", author: "- George Bernard Shaw"},
-    {phrase: "Todos nuestros sueños pueden hacerse realidad si tenemos el coraje de perseguirlos.", author: "- Walt Disney"},
-    {phrase: "Algunas personas quieren que suceda, algunos desean que suceda, otros lo hacen posible.", author: "- Michael Jordan"},
-    {phrase: "El riesgo más grande es no tomar ninguno. En un mundo que está cambiando tan rápido, la única estrategia que está garantizada a fracasar es no tomar riesgos.", author: "- Mark Zuckerberg"},
-    {phrase: "La mejor manera de aprender sobre cualquier cosa es haciéndolo.", author: "- Richard Branson"},
-    {phrase: "Si siempre haces lo que te interesa, al menos una persona estará contenta.", author: "- Katharine Hepburn"},
-    {phrase: "Nada grande se ha logrado sin entusiasmo.", author: "- Ralph Waldo Emerson"},
-    {phrase: "Los que dicen que es imposible no deberían molestar a los que lo estan intentando.", author: "- Albert Einstein"}
+    {phrase: "Sólo vives una vez, pero si lo haces bien, una vez es suficiente.",author: "Mae West"}, 
+    {phrase: "Tu tiempo es limitado, así que no lo desperdicies viviendo la vida de otra persona.", author:"Steve Jobs"},
+    {phrase: "Vive como si fueras a morir mañana. Aprende como si fueras a vivir para siempre.", author: "Mahatma Gandhi"},
+    {phrase: "No cuentes tus días, haz que tus días cuenten.", author: "Muhammad Ali"},
+    {phrase: "La vida no se trata de encontrarte a ti mismo. La vida se trata de crearte a ti mismo.", author: "George Bernard Shaw"},
+    {phrase: "Todos nuestros sueños pueden hacerse realidad si tenemos el coraje de perseguirlos.", author: "Walt Disney"},
+    {phrase: "Algunas personas quieren que suceda, algunos desean que suceda, otros lo hacen posible.", author: "Michael Jordan"},
+    {phrase: "El riesgo más grande es no tomar ninguno. En un mundo que está cambiando tan rápido, la única estrategia que está garantizada a fracasar es no tomar riesgos.", author: "Mark Zuckerberg"},
+    {phrase: "La mejor manera de aprender sobre cualquier cosa es haciéndolo.", author: "Richard Branson"},
+    {phrase: "Si siempre haces lo que te interesa, al menos una persona estará contenta.", author: "Katharine Hepburn"},
+    {phrase: "Nada grande se ha logrado sin entusiasmo.", author: "Ralph Waldo Emerson"},
+    {phrase: "Los que dicen que es imposible no deberían molestar a los que lo estan intentando.", author: "Albert Einstein"}
 ]
 
 newQuote.addEventListener("click", changeAll)
@@ -37,11 +37,11 @@ function changeAll(){
         element.style.color = color;
     })
     let newPhrase = arrPhrases[aleatorio].phrase;
-    let newAuthor =  arrPhrases[aleatorio].author;
+    let newAuthor = arrPhrases[aleatorio].author;
     changePhrase.textContent = newPhrase;
-    changeAuthor.innerText = newAuthor;
+    changeAuthor.innerText = "- " + newAuthor;
     shareTwitter.setAttribute("href","https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="
-                                +newPhrase + " "+ newAuthor);
+                                +newPhrase + " - "+ newAuthor);
     shareTumblr.setAttribute("href", 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' 
                                     + newAuthor +'&content=' + newPhrase 
                                     +'&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button')
